878630d76dd439d75bd666c1a5e2d1dbc4b3ebec
DFernandezSalas <0301dfernandez@e-itaca.es>
2025-11-20 14:50:55 +0100

    Jwt completado

--- ProyectoOrderFlow/TiendaDavid.Identity/Controllers/AuthController.cs
+++ ProyectoOrderFlow/TiendaDavid.Identity/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Asp.Versioning;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TiendaDavid.Identity.Services;
 
 namespace TiendaDavid.Identity.Controllers
 {
@@ -12,12 +13,14 @@ namespace TiendaDavid.Identity.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<AuthController> _logger;
         private readonly IConfiguration _configuration;
+        private readonly IJwtTokenService _jwtTokenService;
 
-        public AuthController(UserManager<IdentityUser> userManager, ILogger<AuthController> logger, IConfiguration configuration)
+        public AuthController(UserManager<IdentityUser> userManager, ILogger<AuthController> logger, IConfiguration configuration, IJwtTokenService jwtTokenService)
         {
             _userManager = userManager;
             _logger = logger;
             _configuration = configuration;
+            _jwtTokenService = jwtTokenService;
         }
 
         [HttpPost("register")]
@@ -49,19 +52,58 @@ namespace TiendaDavid.Identity.Controllers
             });
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponse>> Login(LoginRequest request)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, request.Password))
+            {
+                _logger.LogWarning("Invalid email or password");
+                return Unauthorized(new LoginResponse()
+                {
+                    Message = "User not found",
+                    Token = null,
+                    Email = null
+                });
+            }
+
+            var token = _jwtTokenService.GenerateToken(user);
+            _logger.LogInformation("Login succesful");
+            return Ok(new LoginResponse()
+            {
+                Message = "Login succesful",
+                Token = token,
+                Email = request.Email
+            });
+        }
+
 
         public record RegisterRequest()
         {
             public required string UserName { get; set; }
-            public required string Password { get; set; }
             public required string Email { get; set; }
+            public required string Password { get; set; }
+
         }
 
         public record RegisterResponse()
         {
-            public string Message { get; set; }
+            public string Message { get; set; } = string.Empty;
             public string? Email { get; set; }
-            public IEnumerable<string> Errors { get; set; }
+            public IEnumerable<string>? Errors { get; set; }
+        }
+
+        public record LoginRequest()
+        {
+            public required string Email { get; set; }
+            public required string Password { get; set; }
+        }
+
+        public record LoginResponse()
+        {
+            public string Message { get; set; }
+            public string? Token { get; set; }
+            public string? Email { get; set; }
         }
     }
 }
